ebfba3100ccb5caf99c79217795a1862ed16f31f
Kevin Zhang <kzh227@g.uky.edu>
2023-02-04 14:46:58 -0500

    Added NoteData and Chord Prefab
    Also reworked the ChordCollision for the new data structure. There are some new folders as well.

--- Assets/Scripts/ChordManager.cs
+++ Assets/Scripts/ChordManager.cs
@@ -7,9 +7,6 @@ public class ChordManager : MonoBehaviour
 {
     // this class takes an input of the notes and creates the chord objects for the player to navigate
 
-    // subtract 43 to get the note index in the list of notes
-    public static int NOTE_CONVERSION = 43;
-
     [SerializeField] private GameObject _chordPrefab;
     [SerializeField] private GameObject _notePrefab;
     private MusicPlatformGroup _musicPlatformGroup;
@@ -28,11 +25,11 @@ public class ChordManager : MonoBehaviour
     /// Takes in values to generate the chords of the song
     /// </summary>
     /// <param name="chordList">The data received from Michael</param>
-    public void CreateSong(List<Tuple<int, int, int>> chordList)
+    public void CreateSong(List<List<int>> chordList)
     {
         List<GameObject> chordObjects = new List<GameObject>();
         // create a chord for each value in the list
-        foreach (Tuple<int, int, int> chordData in chordList)
+        foreach (List<int> chordData in chordList)
         {
             chordObjects.Add(CreateChord(chordData, 0)); // input index of root note
             // TODO: Set position of chord based on where it will be in the song
@@ -45,18 +42,21 @@ public class ChordManager : MonoBehaviour
     /// <param name="chordData">The tuple values for the chord</param>
     /// <param name="root">Which note is the root note</param>
     /// <returns>The parent gameobject that contains the notes</returns>
-    public GameObject CreateChord(Tuple<int, int, int> chordData, int root)
+    public GameObject CreateChord(List<int> chordData, int root)
     {
         GameObject chordObject = Instantiate(_chordPrefab, transform);
-        _musicPlatformGroup.GetRowHeight(chordData.Item1);
 
-        Vector2 note1Position = Vector2.up * _musicPlatformGroup.GetRowHeight(chordData.Item1 - NOTE_CONVERSION);
-        Vector2 note2Position = Vector2.up * _musicPlatformGroup.GetRowHeight(chordData.Item2 - NOTE_CONVERSION);
-        Vector2 note3Position = Vector2.up * _musicPlatformGroup.GetRowHeight(chordData.Item3 - NOTE_CONVERSION);
+        ChordCollision chord = chordObject.GetComponent<ChordCollision>();
 
-        Instantiate(_notePrefab, note1Position, Quaternion.identity, chordObject.transform);
-        Instantiate(_notePrefab, note2Position, Quaternion.identity, chordObject.transform);
-        Instantiate(_notePrefab, note3Position, Quaternion.identity, chordObject.transform);
+        chord.Root = root;
+
+        for(int i=0; i<chordData.Count; i++)
+        {
+            Vector2 notePosition = Vector2.up * _musicPlatformGroup.GetRowHeight(chordData[i]);
+            NoteData note = Instantiate(_notePrefab, notePosition, Quaternion.identity, chordObject.transform).GetComponent<NoteData>();
+            chord.NoteDatas.Add(note);
+            note.Note = chordData[i];
+        }
 
         return chordObject;
     }
